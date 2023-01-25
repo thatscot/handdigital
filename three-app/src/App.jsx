53a471626beb4f941df650158408385fb3acd9eb
Jordan Antoniou <jordanantoniou@rw69hxqtyj.home>
2023-01-25 10:39:09 +0000

    Added Three Scene with Ground and Sky

--- three-app/src/App.jsx
+++ three-app/src/App.jsx
@@ -1,21 +1,38 @@
 import React, { useRef, useState, useLayoutEffect } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
+import * as THREE from 'three';
+import { Canvas, useLoader, useFrame } from '@react-three/fiber';
 import { VRButton, ARButton, XR, Controllers, Hands } from '@react-three/xr';
-
-import { useLoader } from '@react-three/fiber';
+import { useGLTF, Sky, useTexture, OrbitControls } from '@react-three/drei';
+import { Physics, RigidBody, CuboidCollider, Debug } from "@react-three/rapier";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { useGLTF } from '@react-three/drei';
 
-function Drone(props) {
+import grass from "./assets/grass.png"
+
+const Ground = (props) => {
+  const texture = useTexture(grass);
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+
+  return (
+    <RigidBody {...props} type="fixed" colliders={false}>
+      <mesh receiveShadow position={[0, 0, 0]} rotation-x={-Math.PI / 2}>
+        <planeGeometry args={[1000, 1000]} />
+        <meshStandardMaterial map={texture} map-repeat={[240, 240]} color="green" />
+      </mesh>
+      <CuboidCollider args={[1000, 2, 1000]} position={[0, -2, 0]} />
+    </RigidBody>
+  )
+};
+
+const Drone = (props) => {
   const gltf = useLoader(GLTFLoader, '/file-1592658408798.glb');
   return (
     <>
       <primitive {...props} object={gltf.scene} scale={1} />
     </>
   );
-}
+};
 
-function Box(props) {
+const Box = (props) => {
   const mesh = useRef(null);
   return (
     <mesh {...props} ref={mesh} scale={1}>
@@ -23,13 +40,21 @@ function Box(props) {
       <meshStandardMaterial color={'orange'} />
     </mesh>
   );
-}
+};
 
 export default function App() {
   return (
     <>
       <VRButton />
-      <Canvas>
+      <Canvas shadows camera={{ fov: 45 }}>
+        <Sky sunPosition={[100, 20, 100]} />
+        <ambientLight intensity={0.3} />
+        <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
+        <Physics >
+          <Ground />
+          <Debug />
+        </Physics>
+        <OrbitControls />
         <XR>
           <ambientLight />
           <pointLight position={[10, 10, 10]} />
