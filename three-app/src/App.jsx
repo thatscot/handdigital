5591f7f1dcdfee2afaedff983fbec3d0615ab5be
Calum Scott <calum.scott@and.digital>
2023-01-25 10:57:45 +0000

    Removing box component and wrapping rest of stuff in VR

--- three-app/src/App.jsx
+++ three-app/src/App.jsx
@@ -3,10 +3,10 @@ import * as THREE from 'three';
 import { Canvas, useLoader, useFrame } from '@react-three/fiber';
 import { VRButton, ARButton, XR, Controllers, Hands } from '@react-three/xr';
 import { useGLTF, Sky, useTexture, OrbitControls } from '@react-three/drei';
-import { Physics, RigidBody, CuboidCollider, Debug } from "@react-three/rapier";
+import { Physics, RigidBody, CuboidCollider, Debug } from '@react-three/rapier';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
-import grass from "./assets/grass.png"
+import grass from './assets/grass.png';
 
 const Ground = (props) => {
   const texture = useTexture(grass);
@@ -16,11 +16,15 @@ const Ground = (props) => {
     <RigidBody {...props} type="fixed" colliders={false}>
       <mesh receiveShadow position={[0, 0, 0]} rotation-x={-Math.PI / 2}>
         <planeGeometry args={[1000, 1000]} />
-        <meshStandardMaterial map={texture} map-repeat={[240, 240]} color="green" />
+        <meshStandardMaterial
+          map={texture}
+          map-repeat={[240, 240]}
+          color="green"
+        />
       </mesh>
       <CuboidCollider args={[1000, 2, 1000]} position={[0, -2, 0]} />
     </RigidBody>
-  )
+  );
 };
 
 const Drone = (props) => {
@@ -32,35 +36,23 @@ const Drone = (props) => {
   );
 };
 
-const Box = (props) => {
-  const mesh = useRef(null);
-  return (
-    <mesh {...props} ref={mesh} scale={1}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={'orange'} />
-    </mesh>
-  );
-};
-
 export default function App() {
   return (
     <>
       <VRButton />
       <Canvas shadows camera={{ fov: 45 }}>
-        <Sky sunPosition={[100, 20, 100]} />
-        <ambientLight intensity={0.3} />
-        <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
-        <Physics >
-          <Ground />
-          <Debug />
-        </Physics>
-        <OrbitControls />
         <XR>
+          <Sky sunPosition={[100, 20, 100]} />
+          <ambientLight intensity={0.3} />
+          <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
+          <Physics>
+            <Ground />
+            <Debug />
+          </Physics>
+          <OrbitControls />
           <ambientLight />
           <pointLight position={[10, 10, 10]} />
           <Drone position={[1, 0, 3]} />
-          <Box position={[5, 0, 0]} />
-          <Box position={[10, 0, 0]} />
         </XR>
       </Canvas>
     </>
