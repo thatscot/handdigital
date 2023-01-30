dbdcd3566685dfed1b89568f47f0b58ceb99c2f2
Calum Scott <calum.scott@and.digital>
2023-01-30 16:50:57 +0000

    Adding backend url to three-app

--- three-app/src/utils/sockets.js
+++ three-app/src/utils/sockets.js
@@ -1,35 +1,35 @@
 import { io } from 'socket.io-client';
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http:localhost:3000';
 
 let socket;
 
 export const onConnect = (setIsConnected) => {
-    socket.on('connect', () => {
-        console.log('Socket Connected ...');
-        setIsConnected(true);
-    });
+  socket.on('connect', () => {
+    console.log('Socket Connected ...');
+    setIsConnected(true);
+  });
 };
 
 export const onDisconnect = (setIsConnected) => {
-    socket.on('disconnect', () => {
-        console.log('Socket Disconnected ... ...');
-        setIsConnected(false);
-    });
+  socket.on('disconnect', () => {
+    console.log('Socket Disconnected ... ...');
+    setIsConnected(false);
+  });
 };
 
 export const initiateSocketConnection = () => {
-    console.log(`Connecting socket...`);
-    socket = io('http://localhost:3000');
+  console.log(`Connecting socket...`);
+  socket = io(BACKEND_URL);
 };
 
 export const disconnectSocket = () => {
-    console.log('Disconnecting socket...');
-    if (socket) socket.disconnect();
+  console.log('Disconnecting socket...');
+  if (socket) socket.disconnect();
 };
 
 export const onMessageHandler = (setAction) => {
-
-    socket.on('message', ({ name, lifecycle }) => {
-        console.log({ command: { name, lifecycle } });
-        setAction({ name, lifecycle });
-    });
+  socket.on('message', ({ name, lifecycle }) => {
+    console.log({ command: { name, lifecycle } });
+    setAction({ name, lifecycle });
+  });
 };
