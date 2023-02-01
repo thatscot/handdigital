bc4ba90ec7c27e27db96b91d337bc65e759116c1
Jordan Antoniou <jordanantoniou@rw69hxqtyj.home>
2023-02-01 11:32:33 +0000

    changed timer styling and position

--- three-app/src/components/Interface.jsx
+++ three-app/src/components/Interface.jsx
@@ -7,12 +7,12 @@ export const Interface = () => {
 
     const { gameState, startGame, resetGame } = useGameContext();
 
-    // const { elapsedTime } = useElapsedTime({ isPlaying: isGameStarted && !isGameCompleted });
+    const { elapsedTime } = useElapsedTime({ isPlaying: gameState === GAME_STATE.STARTED });
 
 
     return <div className="interface">
-        {gameState === GAME_STATE.LOADED ? <div className="hud" onClick={() => startGame()}>START</div> : null}
-        {/* {isGameStarted ? <div className="hud"> {elapsedTime.toFixed(1)} </div> : null} */}
-        {gameState === GAME_STATE.COMPLETED ? <div className="hud" onClick={() => resetGame()}>RESTART</div> : null}
+        {gameState === GAME_STATE.LOADED ? <div className="menu" onClick={() => startGame()}>START</div> : null}
+        {gameState === GAME_STATE.COMPLETED ? <div className="menu" onClick={() => resetGame()}>RESTART</div> : null}
+        <div className="time"> {elapsedTime.toFixed(1)} </div>
     </div>;
 };
