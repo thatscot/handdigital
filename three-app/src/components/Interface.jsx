e322871e8e57fd8157f4fe60554ce4c96bb9f78b
Jamie Moyes <jamiecmoyes@gmail.com>
2023-02-03 16:19:36 +0000

    refactor interface button and add space key listener

--- three-app/src/components/Interface.jsx
+++ three-app/src/components/Interface.jsx
@@ -1,12 +1,45 @@
+import { useEffect } from "react";
 import { useGameContext } from "../hooks";
-import { GAME_STATE } from '../utils/constants';
+import { GAME_STATE } from "../utils/constants";
 
 export const Interface = () => {
+  const { gameState, startGame, resetGame } = useGameContext();
 
-    const { gameState, startGame, resetGame } = useGameContext();
+  const shouldDisplayButton = [
+    GAME_STATE.LOADED,
+    GAME_STATE.COMPLETED,
+  ].includes(gameState);
 
-    return <div className="interface">
-        {gameState === GAME_STATE.LOADED ? <div className="menu" onClick={() => startGame()}>START</div> : null}
-        {gameState === GAME_STATE.COMPLETED ? <div className="menu" onClick={() => resetGame()}>RESTART</div> : null}
-    </div>;
+  if (!shouldDisplayButton) return <div />;
+
+  let text, action;
+
+  switch (gameState) {
+    case GAME_STATE.LOADED: {
+      text = "START";
+      action = startGame;
+      break;
+    }
+    case GAME_STATE.COMPLETED: {
+      text = "RESTART";
+      action = resetGame;
+      break;
+    }
+  }
+
+  useEffect(() => {
+    window.onkeydown = (e) => {
+      if (!e.repeat && e.code === "Space") {
+        action();
+      }
+    };
+  });
+
+  return (
+    <div className="interface">
+      <div className="menu" onClick={action}>
+        {text}
+      </div>
+    </div>
+  );
 };
