0d9aaa4822db758f6a562d6a121674bb13acc267
Jordan Antoniou <jordanantoniou@RW69HXQTYJ.local>
2023-02-03 09:50:26 +0000

    remove debug

--- three-app/src/App.jsx
+++ three-app/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { KeyboardControls } from "@react-three/drei";
-import { Physics, Debug } from "@react-three/rapier";
+import { Physics } from "@react-three/rapier";
 import { Lights } from "./components/Lights";
 import { Level } from "./components/Level";
 import { Player } from "./components/Player";
@@ -19,7 +19,6 @@ export default function App() {
             <Lights />
             <Level />
             <Player />
-            <Debug />
           </Physics>
         </XR>
       </Canvas>
